786f731621a72b539634866fbf833ed5aa22aa81
ruan oliveira <ruanfelipeoliveira@hotmail.com>
2023-05-10 23:34:04 +0000

    Criando arquivos na base de dados

---
+++ src/main/java/model/Autor.java
@@ -0,0 +1,12 @@
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import javax.persistence.Id;
+
+
+@Entity
+@Table(name = "autores")
+
+public class Autor{
+
+
+}
---
+++ src/main/java/model/Livro.java
@@ -0,0 +1,13 @@
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import javax.persistence.Id;
+
+
+@Entity
+@Table(name = "livros")
+
+
+public class Livro{
+
+
+}
